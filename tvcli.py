2215678514fee6fecd0f33bbb9ae54fab9dab98c
Adam Tonks <adam@adam-tonks.co.uk>
2011-02-03 21:35:51 +0000

    Started search function.

--- tvcli.py
+++ tvcli.py
@@ -8,18 +8,37 @@ This is the first project I've created in python, feedback is welcome.
 Copyright (c) Adam tonks 2011
 """
 
-import sys, getopt
+import getopt, urllib, sys
+from xml.dom.minidom import parse, parseString
+
+def search(program):
+    sock = urllib.urlopen("http://www.thetvdb.com/api/GetSeries.php?seriesname="+program)
+    f = sock.read()
+    sock.close()
+    dom = parseString(f)
+    
+    if len(dom.getElementsByTagName('Series')) == 0:
+        print "No episodes found with that name."
+        return 1;
+
+    
 
 def main(argv):
-    if len(argv) == 1:
+
+    if len(argv) == 0:
         usage()
         sys.exit(1)
 
     try:
         opts, args = getopt.getopt(argv, "s:a:d:tT", ["search=","add=","days=","today","tomorrow"])
+
     except getopt.GetoptError:
         usage()
-        sys.exit(2)
+        sys.exit(2) 
+
+    for o, a in opts:
+        if o in ("-s", "--search"):
+            search(a)
 
 def usage():
     print "Usage: tvcli <action>"
@@ -28,4 +47,4 @@ def usage():
     print "   -d,\t--days=NUM\t\tList programs airing in the next NUM days."
     print "   -t,\t--today\t\t\tList programs airing today."
     print "   -T,\t--tomorrow\t\tList programs airing tomorrow."
-main(sys.argv)
+main(sys.argv[1:])
