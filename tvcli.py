864cda58008a231546adb4fe9203f2e69eb6372c
Adam Tonks <adam@adam-tonks.co.uk>
2011-02-05 12:06:44 +0000

    Adding coloring, delete function and more info to info function.

--- tvcli.py
+++ tvcli.py
@@ -11,6 +11,8 @@ Copyright (c) Adam Tonks 2011
 import copy, getopt, os.path, pickle, urllib, sys, time
 from datetime import date
 from xml.dom.minidom import parse, parseString
+from colorama import init, Fore, Back, Style
+init()
 
 APIKEY = 'AB4A43DCDF3A99B5'
 
@@ -44,7 +46,7 @@ def listProgs(favs):
         print "Your favourites list is empty :("
         return 1
     for i in range(len(favs)):
-        print `i+1`+': '+favs[i]['SeriesName']
+        print Fore.CYAN+Style.BRIGHT+`i+1`+': '+Style.RESET_ALL+favs[i]['SeriesName']
     return 0
 
 def getNextEp(pID):
@@ -99,11 +101,29 @@ def search(program):
         if len(name) < 0:
             break
 
-        print "\n"+ID[0].childNodes[0].data+" - "+name[0].childNodes[0].data
-        if len(fa) > 0:
-            print "   First Aired:\t"+fa[0].childNodes[0].data
+        s = getSeriesData(ID[0].childNodes[0].data)
+
+        print "\n"+Style.BRIGHT+Fore.BLUE+ID[0].childNodes[0].data+" - "+name[0].childNodes[0].data+Style.RESET_ALL
         if len(overview) > 0:
-            print "   Overview:\t"+overview[0].childNodes[0].data[:60].rsplit(' ',1)[0]+"..."
+            print Style.BRIGHT+"   Overview:\t"+Style.NORMAL+overview[0].childNodes[0].data[:60].rsplit(' ',1)[0]+"..."
+        if len(fa) > 0:
+            print Style.BRIGHT+"   First Aired:\t"+Style.NORMAL+date.fromtimestamp(time.mktime(time.strptime(fa[0].childNodes[0].data,"%Y-%m-%d"))).strftime("%d %B %Y")
+        if 'Genre' in s:
+            genres = s['Genre'].split('|')
+            print Style.BRIGHT+"   Genre(s):\t"+Style.NORMAL,
+            for i in range(len(genres)):
+                genre = genres[i]
+                if len(genre.strip()) > 0:
+                    sys.stdout.write(genre)
+                    if len(genres) != (i+2):
+                        print ", ",
+            print
+        if 'Network' in s:
+            print Style.BRIGHT+"   Network:\t"+Style.NORMAL+s['Network']
+        if 'Rating' in s:
+            print Style.BRIGHT+"   Rating:\t"+Style.NORMAL+s['Rating']
+        if 'Status' in s:
+            print Style.BRIGHT+"   Status:\t"+Style.NORMAL+s['Status']
 
 def updateEpisodes(pID):
     url = "http://www.thetvdb.com/api/"+APIKEY+"/series/"+pID+"/all"
@@ -128,10 +148,25 @@ def updateEpisodes(pID):
 
     f = open('data/episodes/'+pID+'.pk','w')
     pickle.dump(episodes,f)
-    f.close
+    f.close()
 
-def add(pID):
-    print "Searching for "+pID+" in the TVDB..."
+def delete(pID):
+    d = getData()
+    print Style.BRIGHT+"Are you sure you want to delete "+d[int(pID)-1]['SeriesName']+"?"+Style.NORMAL
+    i = raw_input('(y/n): ')
+    if i == "n" or i=="N":
+        print "Quitting..."
+        sys.exit(1)
+    elif i!="y" and i!="Y":
+        print "Invalid."
+        sys.exit(1)
+    del d[int(pID)-1]
+    f = open('data/favourites.pk','w')
+    pickle.dump(d,f)
+    f.close()
+    print "Deleted."
+
+def getSeriesData(pID):
     url = "http://www.thetvdb.com/api/"+APIKEY+"/series/"+pID+"/all"
     sock = urllib.urlopen(url)
     if sock.getcode() == 200:
@@ -143,14 +178,21 @@ def add(pID):
     dom = parseString(f)
     
     prog = {}
-    progs = []
 
     for sInfo in dom.getElementsByTagName('Series'):
         for tag in sInfo.childNodes:
             if tag.nodeName != "#text" and tag.nodeValue != "\n":
                 if len(tag.childNodes) > 0:
                     prog[tag.nodeName] = tag.childNodes[0].data
+    return prog
+
+
+def add(pID):
+    print "Searching for "+pID+" in the TVDB..."
     
+    prog = getSeriesData(pID)
+
+    progs = []
     if os.path.isfile('data/favourites.pk'):
         current = getData()
         current.append(prog)
@@ -178,10 +220,10 @@ def info(fID):
 
     program = d[fID]
 
-    print `fID+1`+": "+program['SeriesName']
-    print "  Airs: "+program['Airs_DayOfWeek']+"s "+program['Airs_Time']+" (Network's local time)"
-    print "  Latest Episode: "+getLastEp(program['id'])
-    print "  Next Episode: "+getNextEp(program['id'])
+    print Fore.BLUE+Style.BRIGHT+`fID+1`+": "+program['SeriesName']+Style.RESET_ALL
+    print Style.BRIGHT+"  Airs: "+Style.NORMAL+program['Airs_DayOfWeek']+"s "+program['Airs_Time']+" (Network's local time)"
+    print Style.BRIGHT+"  Latest Episode: "+Style.NORMAL+getLastEp(program['id'])
+    print Style.BRIGHT+"  Next Episode: "+Style.NORMAL+getNextEp(program['id'])
 
 def usage():
     print "Usage: tvcli <action>"
@@ -193,6 +235,7 @@ def usage():
     print "   -T,\t--tomorrow\t\tList programs airing tomorrow."
     print "   -u,\t--update\t\tUpdate episode lists."
     print "   -l,\t--list\t\t\tList the programs being tracked."
+    print "   -D,\t--delete=PID\t\tDelete a program using ID from --list."
 
 def main(argv):
 
@@ -201,7 +244,7 @@ def main(argv):
         sys.exit(1)
 
     try:
-        opts, args = getopt.getopt(argv, "s:i:a:d:tTul", ["search=","info=","add=","days=","today","tomorrow","update","list"])
+        opts, args = getopt.getopt(argv, "s:i:a:d:D:tTul", ["search=","info=","add=","days=","delete=","today","tomorrow","update","list"])
 
     except getopt.GetoptError:
         usage()
@@ -218,5 +261,7 @@ def main(argv):
             add(a)
         if o in ("-i", "--info"):
             info(a)
+        if o in ("--delete"):
+            delete(a)
 
 main(sys.argv[1:])
